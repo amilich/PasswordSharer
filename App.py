2c7d013c3da030e6c4ec61d981017e220b7b75a0
Andrew <milichab@gmail.com>
2015-10-18 12:54:40 -0700

    user management - added user class, login system, form validation; also added mandrill

--- App.py
+++ App.py
@@ -1,10 +1,67 @@
-from flask import Flask, Markup, session
+from flask import Flask, Markup, session, Response, render_template, request
 from flask.ext.mandrill import Mandrill
+from flask.ext.login import LoginManager, UserMixin, login_required
+from forms import loginform
 
 app = Flask(__name__)
+
+# login config
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+# Mandrill config
 app.config['MANDRILL_API_KEY'] = 'AlGPWLcyBN97zbLs59HcKw'
 app.config['MANDRILL_DEFAULT_FROM'] = 'noreply@passwordsharer.com'
 mandrill = Mandrill(app)
+
+# we need a better secret key 
+app.config['SECRET_KEY'] = '123456790'
+
+# database config
+# app.config['DATABASE_FILE'] = 'sample_db.sqlite'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + app.config['DATABASE_FILE']
+# app.config['SQLALCHEMY_ECHO'] = True
+# db = SQLAlchemy(app)
+
+
+user_database = {1: ("JohnDoe@johnthebomb.com", "John", 1),
+               2: ("Jane@aol.com", "Jane", 2)} # temporary database
+
+class User(UserMixin):
+    def __init__(self, name, id, active=True):
+        self.name = name
+        self.id = id
+        self.active = active
+
+    def is_active(self):
+        # Here you should write whatever the code is
+        # that checks the database if your user is active
+        	# Andrew: ie email confirmed
+        return self.active
+
+    def is_anonymous(self):
+        return False
+
+    def is_authenticated(self):
+        return True
+
+@login_manager.user_loader
+def load_user(id):
+    # 1. Fetch against the database a user by `id` 
+    # 2. Create a new object of `User` class and return it.
+    # ultimately: u = DBUsers.query.get(id)
+    return User(u.name,u.id,u.active)
+
+@app.route("/login", methods=["GET","POST"])
+def login():
+	form = loginform()
+    if form.validate_on_submit():
+        # do something
+    return render_template("login.html", form=form)
+
+@app.route("/",methods=["GET"])
+def index():
+    return "hi"
 
 @app.route("/testmail")
 def testmail():
@@ -14,15 +71,5 @@ def testmail():
     to=[{'email': 'useremail@useremail.com'}],
     text=Markup('Your invite to password sharer'))
 
-@app.route("/hello")
-def hello():
-    return "Andrew!"
-
-@app.route("/")
-def slash():
-	return "Hello"
-	return render_template("static/login.html")
-
 if __name__ == "__main__":
-    app.run()
-    app.debug = True
+    app.run(debug=True)
